a823e71ee618bf71e88c09c5b95bd395ba3ebcf9
Ndumiso <ndumisomafokoane4@gmail.com>
2025-05-26 13:17:02 +0200

    Update 4-concat.js

---
+++ 4-concat.js
@@ -0,0 +1,7 @@
+const [first, second] = process.argv.slice(2);
+
+if (first === undefined || second === undefined) {
+  console.log("Two arguments are required");
+} else {
+  console log( ${first} is ${second} ); 
+}

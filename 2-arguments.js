8da875dae74d502954ba787f9f210b68708b65a3
Ndumiso <ndumisomafokoane4@gmail.com>
2025-05-26 13:54:37 +0200

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,7 @@
-const args = process.argv.slice(2); //Removes 'node' and script path, keeps only arguments
+const args = process.argv.slice(2); // Keeps only command line arguments
+
 if (args.length === 0) {
   console.log("No argument");
-} else if (args.length === 1) {
-  console.log("Arguments found");
 } else {
-  console.log("Arguments found");
+  console.log("Argument 1:", args[0]);
 }
